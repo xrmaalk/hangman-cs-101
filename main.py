9f21a106192b6b19d5ce8848210bdb3b238ec4be
MKXinrare <33601035+xrmaalk@users.noreply.github.com>
2023-01-17 07:17:00 -0700

    Added Play again Functionality and encapsulation

--- main.py
+++ main.py
@@ -1,58 +1,91 @@
-#Hangman Game
+# Hangman Game - MK Xinra 100 Days of Code Challenge - Udemy - london appbrewy
+# CS-101 Final Project Version -
+#Codecademy.com
 import random
 from hangman_art import logo, stages
 import hangman_words
 from clear import clear
 
+clear()
 print(logo)
+class HangmanGame:
+    def __init__(self):
+        self.lives = 6
+        self.chosen_word = random.choice(hangman_words.word_list)
+        self.end_of_game = False
+        self.display = []
+        self.list_char = list(self.chosen_word)
+        self.word_length = len(self.chosen_word)
+        self.char_reduction = self.word_length
+        self.used_letters = []
+        for _ in range(self.word_length):
+            self.display += "_"
+            
+    def play(self):
+        while not self.end_of_game:
+            user_guess = input("Enter a letter between a - z: \n ").lower()
+            if user_guess in self.display:
+                print("You've already used that letter!")
 
-lives = 6
-chosen_word = random.choice(hangman_words.word_list)
-end_of_game = False
-display = []
-list_char = list(chosen_word)
-word_length = len(chosen_word)
-char_reduction = word_length
-used_letters = []
-#print(chosen_word)
+            for position in range(self.word_length):
+                letter = self.chosen_word[position]
+                if letter == user_guess:
+                    self.display[position] = letter
 
-for _ in range(word_length):
-    display += "_"
+            if user_guess in self.used_letters:
+                print(f"You've already used that letter!\n See {self.used_letters}")
+            elif user_guess not in self.chosen_word:
 
-while not end_of_game:
-    user_guess = input("Enter a letter between a - z: \n ").lower()
-    #clear()
-    if user_guess in display:
-        print("You've already used that letter!")
+                print(f"Your guess {user_guess} is not in the word. You lose a life.")
+                self.lives -= 1
+                if self.lives < 0:
+                    self.lives = 0
+                self.used_letters += user_guess
+                print(f"Incorrect Letters Used: \n ",self.used_letters)
+                if self.lives == 0:
+                    self.end_of_game = True
+                    print(f"{stages[self.lives]}")
+                    print(f"\nThe word is {self.chosen_word}")
+                    print(f"\nYou LOST")
+                    self.play_again()
 
-    for position in range(word_length):
-        letter = chosen_word[position]
-        if letter == user_guess:
-            display[position] = letter
 
-    if user_guess in used_letters:
-        print(f"You've already used that letter!\n See {used_letters}")
-    elif user_guess not in chosen_word:
+            print(f"{' '.join(self.display)} ")
+            if "_" not in self.display and not self.lives == 0:
+                self.end_of_game = True
+                print(f"The word is {self.chosen_word}")
+                print(f"\nYou WIN!!")
+                self.play_again()
 
-        print(f"Your guess {user_guess} is not in the word. You lose a life.")
-        lives -= 1
-        used_letters += user_guess
-        print(f"Incorrect Letters Used: \n ",used_letters)
-        if lives == 0:
-            end_of_game = True
-            print(f"{stages[lives]}")
-            print(f"\nThe word is {chosen_word}")
-            print(f"\nYou LOST")
-        
 
-    print(f"{' '.join(display)} ")
-    if "_" not in display and not lives == 0:
-        end_of_game = True
-        print(f"The word is {chosen_word}")
-        print(f"\nYou WIN!!")
-        
+            
+            if self.lives > 0:
+                print(stages[self.lives])
+                print(f"{self.display} ")
+            else:
+                print('\nGAME OVER')
+                
 
-    print(stages[lives])
-    print(f"{display} ")
 
-    
+    def play_again(self):
+        self.lives = 6
+        self.chosen_word = random.choice(hangman_words.word_list)
+        self.display = []
+        self.list_char = list(self.chosen_word)
+        self.word_length = len(self.chosen_word)
+        self.char_reduction = self.word_length
+        self.used_letters = []
+        for _ in range(self.word_length):
+            self.display += "_"
+        play_again = input('\n PLAY AGAIN: Y or N \n ')
+        clear()
+        if 'y' in play_again.lower():
+            self.end_of_game = False
+            print(logo)
+            self.play()
+        else:
+            print(logo)
+            print('\nGood-Bye')       
+
+game = HangmanGame()
+game.play()
